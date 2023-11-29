94ce2e710f6d8b55df7b8a0de196fa1323ef1f9d
enrico <enricocalioloo@gmail.com>
2023-11-29 14:14:24 -0300

    arrumada ordem de saída da atração

--- ixphere_thread.py
+++ ixphere_thread.py
@@ -1,6 +1,6 @@
-
 import threading
 from queue import Queue
+from time import sleep
 
 from helper import *
 import global_variables as gv
@@ -18,34 +18,34 @@ class Ixphere(threading.Thread):
         self.aberto = False
         self.clientes_atendidos = 0
         self.pessoas_na_atracao = Queue(maxsize=dados.n_vagas)
-        
+
     def iniciaAtracao(self, pessoa: Pessoa):
         self.aberto = True
         self.atracao = pessoa.faixa_etaria
         print(f"[Ixfera] Iniciando a experiencia {pessoa.faixa_etaria}")
-        
+
     def entrarNaEsfera(self, pessoa: Pessoa):
-            if self.pessoas_na_atracao.full():
+        if self.pessoas_na_atracao.full():
+            q_pessoa: Pessoa = self.pessoas_na_atracao.get()
+            q_pessoa.sem_sair_atracao.acquire()
+
+        self.pessoas_na_atracao.put(pessoa)
+        with gv.mutex_count_pessoas_na_atracao:
+            gv.count_pessoas_na_atracao += 1
+            print(
+                f"[{pessoa}] Entra na Ixfera (quantidade = {gv.count_pessoas_na_atracao})."
+            )
+            pessoa.sem_entrar_atracao.release()
+            sleep(0.1)
+
+        self.clientes_atendidos += 1
+
+        # checando se é a última pessoa a entrar na atração
+        if self.clientes_atendidos == self.dados.n_pessoas:
+            while self.pessoas_na_atracao.qsize() != 0:
                 q_pessoa: Pessoa = self.pessoas_na_atracao.get()
                 q_pessoa.sem_sair_atracao.acquire()
 
-            self.pessoas_na_atracao.put(pessoa)
-            with gv.mutex_count_pessoas_na_atracao:
-                gv.count_pessoas_na_atracao += 1
-                print(
-                    f"[{pessoa}] Entra na Ixfera (quantidade = {gv.count_pessoas_na_atracao})."
-                )
-                
-            self.clientes_atendidos += 1
-            
-            pessoa.sem_entrar_atracao.release()
-
-            # checando se é a última pessoa a entrar na atração
-            if self.clientes_atendidos == self.dados.n_pessoas:
-                while self.pessoas_na_atracao.qsize() != 0:
-                    q_pessoa: Pessoa = self.pessoas_na_atracao.get()
-                    q_pessoa.sem_sair_atracao.acquire()
-                    
     def aguardarAtracaoAcabar(self, pessoa: Pessoa):
         while self.pessoas_na_atracao.qsize() != 0:
             q_pessoa: Pessoa = self.pessoas_na_atracao.get()
@@ -67,7 +67,7 @@ class Ixphere(threading.Thread):
         if not self.aberto:
             self.iniciaAtracao(pessoa)
         if pessoa.faixa_etaria == self.atracao:
-           self.entrarNaEsfera(pessoa)
+            self.entrarNaEsfera(pessoa)
         else:
             self.aguardarAtracaoAcabar(pessoa)
             self.iniciaAtracao(pessoa)
