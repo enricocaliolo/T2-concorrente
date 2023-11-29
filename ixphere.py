69f04b2e55df1231fbe9913b35d2ad93813b60e2
enrico <enricocalioloo@gmail.com>
2023-11-29 11:45:04 -0300

    tratamento de erros nos parametros do programa

--- ixphere.py
+++ ixphere.py
@@ -9,18 +9,28 @@ from global_variables import init
 
 
 def getCLIArguments():
-    n_pessoas = int(argv[1])
-    n_vagas = int(argv[2])
-    permanencia = int(argv[3])
-    max_intervalo = int(argv[4])
-    semente = int(argv[5])
-    unid_tempo = int(argv[6])
+    arguments = [int(arg) for arg in argv[1:]]
 
-    return Dados(n_pessoas, n_vagas, permanencia, max_intervalo, semente, unid_tempo)
+    if 0 in arguments:
+        raise Exception("Nenhum dos parâmetros deve ser zero.")
+
+    if sum(arg < 0 for arg in arguments):
+        raise Exception("Nenhum dos parâmetros deve ser negativo.")
+
+    if len(arguments) != 6:
+        raise Exception(
+            "Argumento(s) faltando. Esteja certo de estar compilando com a estrutura: \n<n_pessoas> <n_vagas> <permanencia> <max_intervalo> <semente> <unid_tempo>"
+        )
+
+    return Dados(*arguments)
 
 
 def main():
-    dados = getCLIArguments()
+    try:
+        dados = getCLIArguments()
+    except Exception as inst:
+        print(inst)
+        return
 
     init()
 
